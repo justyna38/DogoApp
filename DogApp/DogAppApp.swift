f3b4837bf29a851bfa1f1dafcfa285edf92ed19b
justyna38 <warzecha.justyna93@gmail.com>
2024-04-06 15:13:38 +0200

    “project”

---
+++ DogApp/DogAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogAppApp.swift
+//  DogApp
+//
+//  Created by Justyna Warzecha on 06/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct DogAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
